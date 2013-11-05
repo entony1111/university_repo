9397b4942ab1f480756f327cd11684af816c4ae5
User2 <entony1111@gmail.com>
2013-11-05 23:01:19 +0200

    monograph script developed

--- quickstart/public/static/js/index/index.js
+++ quickstart/public/static/js/index/index.js
@@ -1,21 +1,98 @@
 SARM.INDEX = 
 {
-	addAuthor: function ()
+	handleMonographFormatting: function()
 	{
-		if(!$('#author-full-name').val())
+            var compiled = {
+               plcae : $('#monograph-publication-place-publication-input')
+                       .val().substr(0,1),
+               pName : $('#monograph-publication-name-publication-input')
+                       .val(),
+               date : $('#monograph-publication-date').datepicker('getDate'),
+               pages : $('#monograph-publication-pages').val(),
+               mName : $('#monograph-publication-monograph-name-input').val(),
+               authors : [] 
+            };
+            $('#monograph-content-author-list .author-list-item')
+               .each(function(){
+                   var spl = $(this).find('.author-name').text().split(' ');
+                   compiled.authors.push(
+                   {
+                       surname : spl[0],
+                       initials : spl[1]
+                   });
+                //get authors
+            });
+            var res = "";
+            if(compiled.authors.length)
+            {
+                if(compiled.authors.length > 1)
+                {
+                    var authors = compiled.authors[0].initials
+                            +' '+compiled.authors[0].surname+',';
+                    for(var i = 1 ; i < compiled.authors.length; i++)
+                    {
+                        authors = authors +' '+ compiled.authors[i].initials
+                        +' '+compiled.authors[i].surname+',';
+                    }
+                    authors = authors.substr(0, authors.length - 1);
+                    
+                    res = compiled.authors[0].surname+' '
+                    +compiled.authors[0].initials+' '+compiled.mName
+                    +' / '+authors+'. - '
+                    +compiled.plcae+'.: '+compiled.pName+', '+2011+'.-'
+                    +compiled.pages+' c.';
+                    //multiple case
+                }else{
+                    res = compiled.authors[0].surname+' '
+                    +compiled.authors[0].initials+' '+compiled.mName
+                    +' / '+compiled.authors[0].initials+' '
+                    +compiled.authors[0].surname+'. - '
+                    +compiled.plcae+'.: '+compiled.pName+', '+2011+'.-'
+                    +compiled.pages+' c.';
+                    //single case
+                }
+            }
+            $('#output-result').prop('value', res);
+	},
+	handleTextbookFormatting: function()
+	{
+		
+	},
+	handleArticleFormatting: function()
+	{
+		
+	},
+	handleThesisFormatting: function()
+	{
+		
+	},
+	handleEresourceFormatting: function()
+	{
+		
+	},
+	addAuthor: function (ev)
+	{
+	    var  id = ev.closest('.content').attr('id');
+		if(!$('#'+id+'-author-full-name').val())
 		{
 			return;
 		}
-		var item = $('.author-list-template').clone().removeClass('author-list-template').removeClass('hidden').addClass('author-list-item');
-		item.find('.author-name').text($('#author-full-name').val());
-		$('#monograph-author-list').append(item);
-		$('#author-full-name').val('');
+		var item = $('#'+id+' .author-list-template').clone()
+                        .removeClass('author-list-template')
+                        .removeClass('hidden').addClass('author-list-item');
+		item.find('.author-name')
+                        .text($('#'+id+'-author-full-name').val());
+		$('#'+id+'-author-list').append(item);
+		$('#'+id+'-author-full-name').val('');
+                SARM.INDEX.handleMonographFormatting();
 	},
 	deleteAuthor: function (el)
 	{
-		el.closest('.author-list-item').remove();
+		var id = el.closest('.content').attr('id');
+		el.closest('#'+id+' .author-list-item').remove();
+                SARM.INDEX.handleMonographFormatting();
 	},
-	wsitchTab: function (ev)
+	switchTab: function (ev)
 	{
 		$('#input-block-tabs li').removeClass('active');
 		$(ev.currentTarget).addClass('active');
@@ -28,18 +105,19 @@ SARM.init = function ()
 {
 	$('#input-block-tabs li').click(function(ev)
 			{
-				SARM.INDEX.wsitchTab(ev);
+				SARM.INDEX.switchTab(ev);
 			});
 	
 	var nowTemp = new Date();
-	var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
-	
-	$('#monograph-publication-date').datepicker({
-		format: 'dd-mm-yyyy',
-		viewMode: 'years',
-		onRender: function(date) {
-	    	return date.valueOf() > now.valueOf() ? 'disabled' : '';
-	  	}
+	var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(),
+        nowTemp.getDate(), 0, 0, 0, 0);
+	$('.datepicker').datepicker({
+		startView: 2,
+		format: 'mm/dd/yyyy',
+		autoclose: true
+	}).on('changeDate', function()
+	{
+		SARM.INDEX.handleMonographFormatting();
 	});
-	$('#monograph-publication-date').datepicker('setValue', new Date());
+	$('.datepicker').datepicker('setDate', new Date());
 };
